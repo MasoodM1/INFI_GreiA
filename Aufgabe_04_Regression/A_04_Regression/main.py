aaacee0c057846b3aa36dcf219091b4a0cdfa5e3
Masood <104983434+MasoodM1@users.noreply.github.com>
2024-11-28 16:21:24 +0100

    A04_Regression

--- Aufgabe_04_Regression/A_04_Regression/main.py
+++ Aufgabe_04_Regression/A_04_Regression/main.py
@@ -5,9 +5,21 @@ import statsmodels.api as sm
 
 df = pd.read_excel('data/bev_meld.xlsx')
 
+print(df.columns)
+
+bezirk = df.iloc[:, 0]
+gemnr = df.iloc[:, 1]
+gemeinde = df.iloc[:, 2]
+jahre = df.iloc[:, 3:]
+
 # A 2
 # A 2.1
-bezirk = df['Bezirk']
-gemnr = df['Gemnr']
-gemeinde = df['Gemeinde']
-years = df.loc[:, '1993':'2021']
+jahre_sum = df.iloc[:, 3:].sum(axis=0)
+
+plt.figure(figsize=(10, 6))
+jahre_sum.plot(kind='line')
+plt.title('Summe der Bevölkerung pro Jahr')
+plt.xlabel('Jahre')
+plt.ylabel('Bevölkerung')
+plt.grid(True)
+plt.show()
